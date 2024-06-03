b3925e4ce270fdcd9c13112acf84371c302de156
SolarSpectre <josephcaza32@gmail.com>
2024-06-02 19:20:20 -0500

    Instancio clase Figuras3D en el main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        Figuras3D cubo = new Figuras3D(12, "Cubo", 6, 3);
+        cubo.mostrar_datos();
+        System.out.println("Volumen: " + cubo.calcular_volumen(cubo.arista, cubo.ncaras));
     }
 }
