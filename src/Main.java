4db42a706be31f9fde56b7e34a7d797e0c40079d
Isaac <150483151+Isaac-Valenzuela@users.noreply.github.com>
2024-06-02 21:03:38 -0500

    Update Main.java
    objetos de Figuras 2D e irregulares agregadas al main :)

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ public class Main {
         Figuras3D cubo = new Figuras3D(12, "Cubo", 6, 3);
         cubo.mostrar_datos();
         System.out.println("Volumen: " + cubo.calcular_volumen(cubo.arista, cubo.ncaras));
+        
+        Figuras2D f1 = new Figuras2D(2,"Cuadrado", 10, 10);
+        f1.mostrar_datos();
+        irregulares i1 = new irregulares(3, "Cuadrado", 23.3, 12.2, "no");
+        i1.Verficiar_irregular();
+        i1.mostrar_datos();
+        
     }
 }
